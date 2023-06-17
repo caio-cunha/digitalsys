a8b2d8ed8969eafa345988f6e93a02ab416cc03e
caio <caiocomputacao2014@gmail.com>
2023-06-17 12:09:36 -0300

    add frontend for loans

--- app/loans/urls.py
+++ app/loans/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from loans.api import urls
+from .views import index
 
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -17,6 +18,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', index, name='index'),
     path('api/', include(urls)),
     path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
